aa4e79d7a321913b8c29dafc5b71a1db14a65d3b
Matthew Wilde <mwilde345@gmail.com>
2017-01-14 20:03:21 -0700

    Built basic format. Finished week 1

---
+++ homeworkSite/client/week1/week1.1.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<div class="panel panel-info">
+  <h3 class="panel-heading">1. From your discrete math textbook (Judith Gersting):
+    Section 3.7, ex. 1, 2, 5, 6, 13 and 26.
+    Section 4.5, ex. 1, 2, 4, 5, 7 and 29.
+  </h3>
+  <div class="panel-body">
+    <div id="imageDiv">
+      <img id="hwImage" src="./Homework1.1.jpg"/>
+    </div>
+  </div>
+</div>
