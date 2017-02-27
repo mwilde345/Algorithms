07e815f2e715be28d2f5a5adb852907970472909
matthewwilde <matthewwilde@ELC310-18.suuad.suu.edu>
2017-02-27 13:39:46 -0700

    Merge branch 'master' of https://github.com/mwilde345/Algorithms.git

--- Algorithms_Eclipse/src/Permutation/Unrank.java
+++ Algorithms_Eclipse/src/Permutation/Unrank.java
@@ -6,33 +6,29 @@ import java.util.ArrayList;
 public class Unrank {
 	public static void main(String[] args){
 		Unrank u = new Unrank();
-		u.unrankMe(new BigInteger("100"),new BigInteger("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"));
+		u.unrankMe(new BigInteger("5"),new BigInteger("57"));
 	}
+	
 	public void unrankMe(BigInteger order, BigInteger rank){
+		
 		ArrayList<BigInteger> notUsed = populateNotUsed(order);
-		ArrayList<BigInteger> permutation = new ArrayList<BigInteger>();
 		BigInteger remainder = rank; //57
 		BigInteger currentBase = order.subtract(BigInteger.ONE); //4
-		int counter = 0;
-		while(notUsed.size()>0){
+		while(remainder.compareTo(BigInteger.ZERO)>0){
 			BigInteger baseFactorial = getFactorial(currentBase); //4!
-			BigInteger sum = baseFactorial.compareTo(remainder)<=0?baseFactorial:BigInteger.ZERO;
-			BigInteger count = sum.compareTo(BigInteger.ZERO)>0?BigInteger.ONE:BigInteger.ZERO;
-			//if do we add the factorials until the remainder is 0? or stop before remainder hits zero and go to the next iteration?
-			while(sum.add(baseFactorial).compareTo(remainder)<=0){
+			BigInteger count = BigInteger.ZERO; //0
+			BigInteger sum = baseFactorial; //24
+			while(sum.add(sum).compareTo(remainder)<0){ //24<57, 48<57
 				count=count.add(BigInteger.ONE);
-				sum=sum.add(baseFactorial);
+				sum=sum.add(sum);
+				//System.out.println(sum.toString());
 			}
-			remainder = remainder.subtract(sum);
+			remainder = rank.subtract(sum);
 			currentBase = currentBase.subtract(BigInteger.ONE);
-			int countIndex = Integer.valueOf(count.toString());
-			if(countIndex<notUsed.size()){
-				permutation.add(notUsed.get(countIndex));
-				notUsed.remove(countIndex);
-			}
+			System.out.println(remainder.toString());
 		}
-		System.out.println(permutation.toString());
 	}
+	
 	public BigInteger getFactorial(BigInteger base){
 		BigInteger fact = BigInteger.ONE;
 		for(BigInteger j = BigInteger.ONE; j.compareTo(base)<=0; j=j.add(BigInteger.ONE)){
@@ -40,11 +36,12 @@ public class Unrank {
 		}
 		return fact;
 	}
+	
 	public ArrayList<BigInteger> populateNotUsed(BigInteger order){
 		ArrayList<BigInteger> result = new ArrayList<BigInteger>();
 		for(BigInteger j = BigInteger.ONE; j.compareTo(order)<=0; j=j.add(BigInteger.ONE)){
 			result.add(j);
-		}
+		}		
 		return result;
 	}
 }
